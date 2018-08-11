dbb7efcdd06a32ed2b261ad81486465cf013c179
gfyoung <gfyoung17@gmail.com>
2018-08-10 23:15:22 -0700

    Add docstrings to JS files

--- src/parseDOM.js
+++ src/parseDOM.js
@@ -1,13 +1,27 @@
 const TIMEOUT = 1000;
 const INVISIBLE = "none";
 
+/**
+ * Get the table element containing the Alexa skills.
+ *
+ * @returns {NodeListOf<Element>}
+ */
 function getSkillTable() {
     return document.getElementsByTagName("tbody");
 }
 
+/**
+ * Wait until the DOM has fully loaded before parsing it.
+ *
+ * We have to use this somewhat hacky way of doing it because checking the
+ * `readyState` of `document` doesn't consistently produce a DOM that has
+ * an available table for extraction.
+ */
 function idleUntilDOMLoaded() {
     const matches = getSkillTable();
 
+    // Check that there is at least one table
+    // available before proceeding.
     if (matches.length === 0) {
         setTimeout(idleUntilDOMLoaded, TIMEOUT);
     } else {
@@ -15,6 +29,12 @@ function idleUntilDOMLoaded() {
     }
 }
 
+/**
+ * Extract the skill name for a row in the Alexa skill table.
+ *
+ * @param skillColumns - A row in the Alexa skill table.
+ * @returns {string} - The skill name.
+ */
 function getSkillName(skillColumns) {
     const nameColumn = skillColumns.item(0);
     const nameSpan = nameColumn.children.item(0);
@@ -25,13 +45,29 @@ function getSkillName(skillColumns) {
     return innerInnerNameSpan.innerText;
 }
 
+/**
+ * Extract the skill status for a row in the Alexa skill table.
+ *
+ * Example statuses might be: "Live" or "In Development"
+ *
+ * @param skillColumns - A row in the Alexa skill table.
+ * @returns {string} - The skill status.
+ */
 function getSkillStatus(skillColumns) {
     const statusColumn = skillColumns.item(skillColumns.length - 2);
     const innerSpan = statusColumn.children.item(0);
     return innerSpan.innerText;
 }
 
+/**
+ * Generate an id that can be used for uniquely identifying table rows.
+ *
+ * @param name - The name of the skill.
+ * @param status - The status of the skill.
+ * @returns {string} - The unique ID for the table entry.
+ */
 function getIdFromNameAndStatus(name, status) {
+    // Sanitize the name and status.
     name = name.replace(":", "");
     name = name.replace(" ", "-");
     status = status.replace(" ", "-");
@@ -39,11 +75,15 @@ function getIdFromNameAndStatus(name, status) {
     return name.toLowerCase() + "-" + status.toLowerCase();
 }
 
+/**
+ * Extract the skills from the DOM and store them in local storage.
+ */
 function getSkills() {
     const skillsList = [];
     const skillTable = getSkillTable().item(0);
     const skillElements = skillTable.children;
 
+    // Tag all rows with unique IDs.
     for (let i = 0; i < skillElements.length; i++) {
         const skillElement = skillElements.item(i);
         const skillColumns = skillElement.children;
@@ -56,7 +96,10 @@ function getSkills() {
         skillsList.push({skillName, skillStatus});
     }
 
+    // Store extracted skills in local storage.
     chrome.storage.sync.set({skillsList});
+
+    // Add message listener for when user wants to toggle visibility.
     chrome.runtime.onMessage.addListener(
         function(request, sender) {
             if (sender.tab) {
@@ -69,13 +112,17 @@ function getSkills() {
                 return;
             }
 
+            // We are now (relatively) certain that we have received a message
+            // from the extension with a skill name and status fields.
             const elementId = getIdFromNameAndStatus(skillName, skillStatus);
             const element = document.getElementById(elementId);
 
+            // There is no element tagged with that ID.
             if (element === null) {
                 return;
             }
 
+            // Toggle the visibility of the element.
             const displayValue = element.style.display;
             element.style.display = displayValue === INVISIBLE ? "" : INVISIBLE;
         });
--- src/popup.js
+++ src/popup.js
@@ -1,10 +1,21 @@
 const LINE_THROUGH = "line-through";
 
+/**
+ * Create an <li> element for display in the popup.
+ *
+ * @param listElement - Configuration for the new list element. Includes
+ *                      a skill name and a skill status.
+ * @returns {HTMLElement} - The newly created list element.
+ */
 function createListElement(listElement) {
+    // Create the node with the associated text.
     const node = document.createElement("li");
     const textNode = document.createTextNode(listElement.skillName +
         " (" + listElement.skillStatus + ")");
 
+    // Add event listener to toggle the visibility of the table row
+    // in the Alexa developer console DOM. Also toggles line through
+    // decoration of the list element in the popup.
     node.appendChild(textNode);
     node.addEventListener("click", function() {
         const textDecoration = this.style.textDecoration;
@@ -27,6 +38,7 @@ chrome.storage.sync.get("skillsList", function(data) {
     const skillList = data.skillsList;
     const skillListTag = document.getElementById("skillList");
 
+    // Extract skills from local storage and append to list.
     for (let i = 0; i < skillList.length; i++) {
         skillListTag.appendChild(createListElement(skillList[i]));
     }
